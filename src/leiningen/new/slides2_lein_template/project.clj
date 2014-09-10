2d76a807a775b0537e88f1fe3d41786e907bc95c
Christian Stamm <christian.stamm@otto.de>
2014-09-10 20:41:21 +0200

    update lib version

--- src/leiningen/new/slides2_lein_template/project.clj
+++ src/leiningen/new/slides2_lein_template/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[slides2 "0.1.0"]
+  :dependencies [[slides2 "0.1.1"]
                  [org.clojure/clojurescript "0.0-2227"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :hooks [leiningen.cljsbuild]
